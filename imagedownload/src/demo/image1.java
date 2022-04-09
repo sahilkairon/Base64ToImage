7fec279c7fba7d2f395cba92bf637bce5d4a8aaf
sahilkairon <sahilkairon66@gmail.com>
2022-04-09 07:11:54 +0530

    Add files via upload

---
+++ imagedownload/src/demo/image1.java
@@ -0,0 +1,28 @@
+package demo;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import demo.*;
+
+
+public class image1 {
+
+
+    public static void main(String[] args) throws IOException {
+        blob obj = new blob();
+
+
+        File f = new File("/Users/sahilkairon/Desktop/demo.jpeg");
+        f.createNewFile();
+        FileOutputStream out = new FileOutputStream(f);
+        BufferedOutputStream buffout = new BufferedOutputStream(out);
+        buffout.write(obj.deb);
+        buffout.flush();
+
+
+
+    }
+
+
+}
